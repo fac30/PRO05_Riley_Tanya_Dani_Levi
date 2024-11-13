ab79e583acc88cb4c9554296497271f138151e0d
GurTati <118075413+gurtatiLND@users.noreply.github.com>
2024-11-13 17:43:23 +0000

    create Login, signup, footer

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,3 +1,14 @@
+import Button from "../components/Button/Button";
+
 export default function Home() {
-    return <h2>Home Page</h2>;
+    return (
+      <div>
+         <h2>Home Page</h2>
+         <Button 
+           label="Click me"
+           onClick={()=> alert('Button clicked!')}
+         />
+      </div>
+      
+    )
   }
--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,3 +1,44 @@
-export default function About() {
-    return <h2>Login Page</h2>;
-  }
+import { useState } from "react";
+import Input from "../components/Input/Input";
+import Button from "../components/Button/Button";
+
+const Login: React.FC = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  return (
+    <div>
+      <h2>
+        Sign up in your account
+      </h2>
+
+      <form>
+        <Input 
+          label="Email"
+          id="email"
+          name="email"
+          type="text"
+          placeholder="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          required
+        />
+        <Input 
+          label="Password"
+          id="password"
+          name="password"
+          type="text"
+          placeholder="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          required
+        />
+        <Button 
+          label="Get started"
+          onClick={()=> alert('Button clicked!')}
+        />
+      </form>
+    </div>   
+  )
+}
+export default Login;
