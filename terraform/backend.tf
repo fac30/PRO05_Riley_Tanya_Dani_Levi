46f93af2375e7d991534dc30b9926b91077b211d
rogers7levi <rogers.levi714@gmail.com>
2024-11-13 22:20:31 +0000

    ec2 instance creation

---
+++ terraform/backend.tf
@@ -0,0 +1,8 @@
+resource "aws_instance" "CookNest_instance" {
+  ami           = var.ami_id
+  instance_type = var.instance_type
+  key_name      = "cooknest"
+  tags = {
+    Name = "cooknest_instance"
+  }
+}
---
+++ terraform/variables.tf
@@ -0,0 +1,10 @@
+variable "ami_id" {
+  default     = "ami-0acc77abdfc7ed5a6"
+  description = "operating system of the instance"
+}
+
+variable "instance_type" {
+  default     = "t2.micro"
+  description = "Power of the proccessor within the instance"
+}
+
